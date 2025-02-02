3a156fae11efc27a268ba83e4e50083dfd961a5c
Satyam Mehta <82329144+merepapa@users.noreply.github.com>
2025-02-02 15:36:57 +0530

    Update API.js

--- API.js
+++ API.js
@@ -2,7 +2,7 @@ require("dotenv").config(); // Load environment variables
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const axios = require("axios");
 
 const API_KEY = process.env.API_KEY;
